16d92bf93d6d3c8b0fcc719bf71906ece5a32e7d
Aung Khant <theaungkhant@gmail.com>
2019-08-02 14:56:07 +0630

    Structuring event-form to be reusable

---
+++ src/productivity/events/containers/event/event.component.ts
@@ -0,0 +1,11 @@
+import { FormGroup } from "@angular/forms";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "event",
+  styleUrls: ["event.component.scss"],
+  template: `
+    <event-form></event-form>
+  `
+})
+export class EventComponent {}
--- src/productivity/events/events.module.ts
+++ src/productivity/events/events.module.ts
@@ -1,13 +1,14 @@
+import { EventComponent } from "./containers/event/event.component";
 import { SharedModule } from "../shared/shared-form.module";
 import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
-import { EventFormComponent } from "./containers/event-form/event-form.component";
+import { EventFormComponent } from "./components/event-form/event-form.component";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatNativeDateModule } from "@angular/material/core";
 
-export const ROUTES: Routes = [{ path: "", component: EventFormComponent }];
+export const ROUTES: Routes = [{ path: "", component: EventComponent }];
 
 @NgModule({
   imports: [
@@ -18,6 +19,6 @@ export const ROUTES: Routes = [{ path: "", component: EventFormComponent }];
     MatDatepickerModule,
     MatNativeDateModule
   ],
-  declarations: [EventFormComponent]
+  declarations: [EventFormComponent, EventComponent]
 })
 export class EventsModule {}
